ed85fef5da659f5c768ce6beef409af86be9019f
Nathan-2008 <86959603+Nathan-2008@users.noreply.github.com>
2021-11-20 15:34:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function player_update() {
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
         player_object.set({
-            top: player_x,
-            left: player_y
+            top: player_y,
+            left: player_x
         });
         canvas.add(player_object);
     });
@@ -23,10 +23,10 @@ function new_image(get_image) {
     fabric.Image.fromURL(get_image, function (Img) {
         block_image_object = Img;
         block_image_object.scaleToWidth(block_image_width);
-        block_image_object.scaleToWidth(block_image_height);
+        block_image_object.scaleToHeight(block_image_height);
         block_image_object.set({
-            top: player_x,
-            left: player_y
+            top: player_y,
+            left: player_x
         });
         canvas.add(block_image_object);
     });
@@ -108,5 +108,45 @@ function my_keydown(e) {
     if (keyPressed == '67') {
         new_image('cloud.jpg');
         console.log("c");
+    }
+}
+
+function up() {
+    if (player_y >= 0) {
+        player_y = player_y - block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When Up arrow key is pressed, X = " + player_x + " , Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down() {
+    if (player_y <= 500) {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " + block_image_height);
+        console.log("When Down arrow key is pressed, X = " + player_x + " , Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left() {
+    if (player_x > 0) {
+        player_x = player_x - block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When Left arrow key is pressed, X = " + player_x + " , Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right() {
+    if (player_x <= 850) {
+        player_x = player_x + block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When Right arrow key is pressed, X = " + player_x + " , Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
     }
 }
